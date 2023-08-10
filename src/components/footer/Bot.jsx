2a515acc9f7201833871fe5e1dbbac5218d57609
gs0428 <lider28@naver.com>
2023-08-10 22:21:09 +0900

    feat : footer 퍼블리싱 완료

--- src/components/footer/Bot.jsx
+++ src/components/footer/Bot.jsx
@@ -1,5 +1,14 @@
 import * as S from "../../styles/footer/bot";
 
 export default function Bot() {
-  return <></>;
+  return (
+    <S.Container>
+      <div>컬리에서 판매되는 상품 중에는 컬리에 입점한 개별 판매자가 판매하는 마켓플레이스(오픈마켓) 상품이 포함되어 있습니다.</div>
+      <div>
+        마켓플레이스(오픈마켓) 상품의 경우 컬리는 통신판매중개자로서 통신판매의 당사자가 아닙니다. 컬리는 해당 상품의 주문, 품질, 교환/환불 등 의무와 책임을
+        부담하지 않습니다.
+      </div>
+      <S.Copyright>© KURLY CORP. ALL RIGHTS RESERVED</S.Copyright>
+    </S.Container>
+  );
 }
--- src/styles/footer/bot.js
+++ src/styles/footer/bot.js
@@ -1,1 +1,20 @@
 import { styled } from "styled-components";
+
+export const Container = styled.div`
+  position: absolute;
+  padding: 20px 0px 30px;
+  left: 0px;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: rgb(247, 247, 247);
+  color: rgb(153, 153, 153);
+  font-size: 10px;
+  line-height: 15px;
+`;
+
+export const Copyright = styled.div`
+  padding-top: 7px;
+`;
